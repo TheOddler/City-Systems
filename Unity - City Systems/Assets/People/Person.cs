00ee64bdf31527225afe3c739ffd9342084bccf5
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-03-05 19:22:02 +0100

    Trying out new navmesh.

---
+++ Unity - City Systems/Assets/People/Person.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Person : MonoBehaviour
+{
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				var agent = GetComponent<NavMeshAgent>();
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
